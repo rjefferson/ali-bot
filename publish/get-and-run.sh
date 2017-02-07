3eb82b5308df5b98e6478ba630f4d0f85d484a02
Jefferson Porter <rjporter@lbl.gov>
2017-02-07 08:24:41 +0100

    place holder updates for Cori

--- publish/get-and-run.sh
+++ publish/get-and-run.sh
@@ -5,6 +5,17 @@ cd "$(dirname "$0")"
 if [[ -x /home/monalisa/bin/alien ]]; then
   export PATH="/home/monalisa/bin:$PATH"
   CMD=sync-alien
+elif [[ -d /afs/cern.ch/user/j/jporter/alicework/ali-bot ]]; then
+  git --help > /dev/null 2>&1
+  FAKECVMFS=/lustre/atlas/proj-shared/csc108/psvirin/publisher/.fakecvmfs
+  mkdir -p $FAKECVMFS
+  ln -nfs $(which true) $FAKECVMFS/cvmfs_server
+  export PATH="$FAKECVMFS:$PATH"
+  [[ ! -e alibuild/.git ]] && git clone https://github.com/alisw/alibuild
+  [[ ! -e requests/.git ]] && git clone https://github.com/kennethreitz/requests -b v2.6.0
+  export PYTHONPATH="$PWD/alibuild:$PWD/requests:$PYTHONPATH"
+  CONF=aliPublish-cori.conf
+  CMD=sync-cvmfs
 elif [[ -d /lustre/atlas/proj-shared/csc108 && -d /lustre/atlas/proj-shared/csc108 ]]; then
   # Titan needs some magic.
   source /usr/share/Modules/init/bash
